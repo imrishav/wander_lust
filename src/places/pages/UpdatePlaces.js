780d24b3832b4c34d540f609010302dbad60f674
Rishav Sinha <rishav@live.co.uk>
2020-03-10 19:21:46 +0530

    removed api key

--- src/places/pages/UpdatePlaces.js
+++ src/places/pages/UpdatePlaces.js
@@ -3,48 +3,47 @@ import { useParams } from "react-router-dom";
 
 const DUMMY_PLACES = [
   {
-    id: 'p1',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
     location: {
       lat: 40.7484405,
       lng: -73.9878584
     },
-    creator: 'u1'
+    creator: "u1"
   },
   {
-    id: 'p2',
-    title: 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
     imageUrl:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg',
-    address: '20 W 34th St, New York, NY 10001',
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
+    address: "20 W 34th St, New York, NY 10001",
     location: {
       lat: 40.7484405,
       lng: -73.9878584
     },
-    creator: 'u2'
+    creator: "u2"
   }
 ];
-
 
 const UpdatePlaces = () => {
   const placeId = useParams().placeId;
 
-  const identifiedPlaces = DUMMY_PLACES.find(p => p.id === placeId)
+  const identifiedPlaces = DUMMY_PLACES.find(p => p.id === placeId);
 
-  if(!identifiedPlaces) {
+  if (!identifiedPlaces) {
     return (
       <div className="center">
         <h2>Could Not find Place!</h2>
       </div>
-    )
+    );
   }
 
-  return <div>Update Place</div>;
+  return <form></form>;
 };
 
 export default UpdatePlaces;
